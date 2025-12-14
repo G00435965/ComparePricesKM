1e93a5a17e597d4bf65f0c298da109f8ecead264
G00435965 <g00435965@atu.ie>
2025-12-14 17:44:42 +0000

    Adding hsitory logs and also comments to help me keep track of everything as some of this material was much different to what we had in the old repository.

--- src/main/java/ie/atu/comparepriceskm/Service/PricesService.java
+++ src/main/java/ie/atu/comparepriceskm/Service/PricesService.java
@@ -11,8 +11,8 @@ import java.util.List;
 @Service
 public class PricesService {
 
-    private final List<PricesInformation> prices = new ArrayList<>(); //linking to Inofrmation
-    private final List<PricesHistory> history = new ArrayList<>(); //Linking to history
+    private final List<PricesInformation> prices = new ArrayList<>(); //stores current prices
+    private final List<PricesHistory> history = new ArrayList<>(); //stores history of prices
 
 
     public List<PricesInformation> getAll() {
@@ -24,28 +24,47 @@ public class PricesService {
     }
 
 
-
+// this is for adding/ updating prices
     public PricesInformation addOrUpdate(PricesInformation newPrice) {
 
-        for (PricesInformation existing : prices) {
+        for (PricesInformation existing : prices) { //this will check the prices
 
-            if (existing.getStationName().equalsIgnoreCase(newPrice.getStationName())
-                    && existing.getFuelType().equalsIgnoreCase(newPrice.getFuelType())) {
+            if (existing.getStationName().equalsIgnoreCase(newPrice.getStationName()) //if the station exists
+                    && existing.getFuelType().equalsIgnoreCase(newPrice.getFuelType())) { //if the fuel type exists
 
                 if (newPrice.getPrice() > existing.getPrice()) {
-                    saveHistory(existing, newPrice, "Price has gone UP");
+                    saveHistory(existing, newPrice, "Price has gone UP"); //checks if the new price is higher than the original one
                 }
                 else if (newPrice.getPrice() < existing.getPrice()) {
-                    saveHistory(existing, newPrice, "Price has gone DOWN");
+                    saveHistory(existing, newPrice, "Price has gone DOWN"); //checks if the new price is lower than the original one
                 }
 
                 existing.setPrice(newPrice.getPrice());
-                return existing;
+                return existing; //returns the updated price
             }
         }
 
         prices.add(newPrice);
         return newPrice;
+    }
+
+    //for recording price change and sending it to the history list.
+    private void saveHistory(PricesInformation oldPrice,
+                             PricesInformation newPrice,
+                             String changeType) {
+
+
+
+        PricesHistory record = new PricesHistory(
+                oldPrice.getStationName(),   // which station
+                oldPrice.getFuelType(),      // petrol or diesel
+                oldPrice.getPrice(),         // old price
+                newPrice.getPrice(),         // new price
+                changeType                  // prices UP or DOWN
+        );
+
+        // this is for recording the history
+        history.add(record);
     }
 
 
