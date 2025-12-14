1778066bb6822cc61987e21001295e13b1d12e4e
G00435965 <g00435965@atu.ie>
2025-12-14 00:55:41 +0000

    Created newest project without all the other dependencies so no errors occur, this is fro comparing fuel station prices.

---
+++ src/main/java/ie/atu/comparepriceskm/ComparePricesKmApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.comparepriceskm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparePricesKmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComparePricesKmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/comparepriceskm/ComparePricesKmApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.comparepriceskm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparePricesKmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
